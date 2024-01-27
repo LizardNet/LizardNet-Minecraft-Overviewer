4a807c51496aa037dbe58482d6f4a230a5978378
Simon Walker <github@stwalkerster.co.uk>
2024-01-27 18:28:47 +0000

    Allow individual markers to override their CSS class

--- overviewer_core/aux_files/genPOI.py
+++ overviewer_core/aux_files/genPOI.py
@@ -407,6 +407,8 @@ def create_marker_from_filter_result(poi, result):
         d["icon"] = poi['icon']
     if "createInfoWindow" in poi:
         d["createInfoWindow"] = poi['createInfoWindow']
+    if "cssClass" in poi:
+        d["cssClass"] = poi['cssClass']
 
     # Fill in the rest from result
     if isinstance(result, str):
